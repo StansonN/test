3133ce16b6726efaa50df3da81e507fb55889733
Stanson Nakanol <snakanol14@gmail.com>
2023-12-11 11:29:06 +1000

    JavaScript
    Testing js code.

---
+++ hello_world.js
@@ -0,0 +1,11 @@
+//Using console.log()
+// the hello world program
+console.log('Hello World');
+
+//Using alert()
+// the hello world program
+alert("Hello, World!");
+
+//Using document.write()
+// the hello world program
+document.write('Hello, World!');
